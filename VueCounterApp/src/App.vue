7b76346d8360b6dc359c9b5c1b5eaa35d99b2735
somogyiova <nela.somogyiova@hollarka.cz>
2023-12-14 19:16:29 +0100

    first commit

---
+++ VueCounterApp/src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+import {ref} from "vue" ;
+const count = ref(0);
+</script>
+
+
+
+<template>
+<main>
+<div>
+  <h4>The current count is...
+  </h4>
+    <h1>
+      {{ count }}
+    </h1>
+    <button @click="count--">-</button>
+    <button @click="count++">+</button>
+  </div>
+</main>
+
+
+</template>
+<style scoped>
+main{
+  background-color: aliceblue;
+  color: black;
+  height: 100vh;
+  width: 100vw;
+  display:flex;
+  align-items: center;
+  justify-content: center;
+
+}
+
+div{
+  text-align: center;
+}
+</style>
